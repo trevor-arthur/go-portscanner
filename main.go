163e5b1f84fd6512342355623bb0f2a4d8ef5c83
trevor-arthur <trevor.spaniola@protonmail.com>
2022-07-08 17:01:24 -0700

    Returned to results

--- main.go
+++ main.go
@@ -9,9 +9,12 @@ import (
 func main() {
 	fmt.Println("[*] Go Port Scanner")
 
-	open := port.ScanPort("tcp", "192.168.0.169", 22)
-	fmt.Printf("Port Open: %t\n", open)
+	// Scans individual port
+	//open := port.ScanPort("tcp", "192.168.0.169", 22)
+	//fmt.Printf("Port Open: %t\n", open)
 
+	// Scans ports 1-1024
 	results := port.InitialScan("192.168.0.169")
 	fmt.Println(results)
+
 }
